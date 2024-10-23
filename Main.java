c44d74aa3a13dfa0e880467066022abee9b98e23
IcebladeJS <54967346+IcebladeJS@users.noreply.github.com>
2024-10-22 21:15:32 -0700

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    MadLib userMadLib = new MadLib();
+    userMadLib.create();
+    userMadLib.clear();
+    userMadLib.fillIn();
+  }
+}
