18e1733dce5ccafc4af1f4e128211dff02e2b9cf
IcebladeJS <54967346+IcebladeJS@users.noreply.github.com>
2024-10-22 21:17:00 -0700

    Updated MadLibs.java

--- MadLibs.java
+++ MadLibs.java
@@ -1,46 +1,94 @@
 import java.util.Scanner;
 
-public class Main {
-  static void createMadLib(int lineNumber) {
+public class MadLib {
+  String firstLineMadLib;
+  int firstLineWordType;
+  
+  String secondLineMadLib;
+  int secondLineWordType;
+  
+  String lastLineMadLib;
+  int lastLineWordType;
+  
+  public void create() {
     String Number;
-    if (lineNumber == 1) {
-      Number = "first";
-    } else if (lineNumber == 2) {
-      Number = "second";
-    } else {
-      Number = "last";
-    }
-    System.out.println("Enter the " + Number + " line of your MadLib, using ? for the missing word.");
     
-    if (lineNumber == 1) {
-      Scanner askFirstLineMadLib = new Scanner(System.in);
-      String firstLineMadLib = askFirstLineMadLib.nextLine();
-    } else if (lineNumber == 2) {
-      Scanner askSecondLineMadLib = new Scanner(System.in);
-      String secondLineMadLib = askSecondLineMadLib.nextLine();
-    } else {
-      Scanner askLastLineMadLib = new Scanner(System.in);
-      String lastLineMadLib = askLastLineMadLib.nextLine();
-    }
-
-    System.out.println("Indicate whether the missing word is a \n1: Noun\n2: Verb\n3: Adjective");
+    for (int lineNumber = 1; lineNumber <= 3; lineNumber++) {
+      
+      if (lineNumber == 1) {
+        Number = "first";
+      } else if (lineNumber == 2) {
+        Number = "second";
+      } else {
+        Number = "last";
+      }
+      
+      System.out.println("Enter the " + Number + " line of your MadLib, using ? for the missing word.");
+        
+      if (lineNumber == 1) {
+        Scanner askFirstLineMadLib = new Scanner(System.in);
+        firstLineMadLib = askFirstLineMadLib.nextLine();
+      } else if (lineNumber == 2) {
+        Scanner askSecondLineMadLib = new Scanner(System.in);
+        secondLineMadLib = askSecondLineMadLib.nextLine();
+      } else {
+        Scanner askLastLineMadLib = new Scanner(System.in);
+        lastLineMadLib = askLastLineMadLib.nextLine();
+      }
     
-    if (lineNumber == 1) {
-      Scanner askFirstLineWordType = new Scanner(System.in);
-      int firstLineWordType = askFirstLineWordType.nextInt();
-    } else if (lineNumber == 2) {
-      Scanner askSecondLineWordType = new Scanner(System.in);
-      int secondLineWordType = askSecondLineWordType.nextInt();
-    } else {
-      Scanner askLastLineWordType = new Scanner(System.in);
-      int lastLineWordType = askLastLineWordType.nextInt();
+      System.out.println("Indicate whether the missing word is a \n1: Noun\n2: Verb\n3: Adjective");
+        
+      if (lineNumber == 1) {
+        Scanner askFirstLineWordType = new Scanner(System.in);
+        firstLineWordType = askFirstLineWordType.nextInt();
+      } else if (lineNumber == 2) {
+        Scanner askSecondLineWordType = new Scanner(System.in);
+        secondLineWordType = askSecondLineWordType.nextInt();
+      } else {
+        Scanner askLastLineWordType = new Scanner(System.in);
+        lastLineWordType = askLastLineWordType.nextInt();
+      }
     }
-
   }
-  public static void main(String[] args) {
-    createMadLib(1);
-    createMadLib(2);
-    createMadLib(3);
+  public void fillIn() {
+    int wordType;
+      
+    for (int i = 0; i < 3; i++) {
+      if (i == 0) {
+        wordType = firstLineWordType;
+      } else if (i == 1) {
+        wordType = secondLineWordType;
+      } else {
+        wordType = lastLineWordType;
+      }
+      if (wordType == 1) {
+        System.out.println("Enter a noun.");
+      } else if (wordType == 2) {
+        System.out.println("Enter a verb.");
+      } else if (wordType == 3) {
+        System.out.println("Enter an adjective");
+      }
+      Scanner askUserFillIn = new Scanner(System.in);
+      String userFillIn = askUserFillIn.nextLine();
+      if (i == 0) {
+        firstLineMadLib = firstLineMadLib.replaceFirst("\\?", userFillIn);
+      } else if (i == 1) {
+        secondLineMadLib = secondLineMadLib.replaceFirst("\\?", userFillIn);
+      } else {
+        lastLineMadLib = lastLineMadLib.replaceFirst("\\?", userFillIn);
+      }
+    }
     
+    this.clear();
+    
+    System.out.println("\nYOUR MADLIB:\n");
+    System.out.println(firstLineMadLib);
+    System.out.println(secondLineMadLib);
+    System.out.println(lastLineMadLib + "\n\n");
+      
+  }
+  
+  public void clear() {
+    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   }
 }
